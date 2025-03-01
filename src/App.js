dbe235fcd134932556e787057609c1b12fc4ab42
Kuwar Singh <kuwarsingh@outlook.com>
2025-03-01 22:09:40 +0530

    Day 01 - Setup a Basic React App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="header">Welcome To React Fundamentals</h1>
+  );
+}
+
+export default App;
